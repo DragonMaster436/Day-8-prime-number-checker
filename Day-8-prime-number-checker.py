82201eae28b558e06f77ed39529c7565a5b1eb8d
DragonMaster436 <96381483+DragonMaster436@users.noreply.github.com>
2024-02-17 20:31:43 -0600

    Add files via upload

---
+++ Day-8-prime-number-checker.py
@@ -0,0 +1,28 @@
+# Write your code below this line 👇
+def prime_checker (number):
+    prime_numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+    if number in prime_numbers:
+        print("It's a prime number.")
+    else:
+        print("It's not a prime number.")
+# Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = int(input()) # Check this number
+prime_checker(number=n)
+
+
+# This is what she did
+
+# def prime_checker(number):
+#   is_prime = True
+#   for i in range(2, number):
+#     if number % i == 0:
+#       is_prime = False
+#   if is_prime:
+#     print("It's a prime number.")
+#   else:
+#     print("It's not a prime number.")   
+# # Your code above this line 👆
+# n = int(input()) # Check this number
+# prime_checker(number=n)
